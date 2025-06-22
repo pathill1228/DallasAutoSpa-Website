0c306b71ec1615d487313536c0523019317570bd
pathill1 <pathill1228@gmail.com>
2025-06-21 20:53:32 -0500

    Navbar test
    testing to see if bootstrap libraries will apply to navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a href="/" className="navbar-brand">
+        Dallas Auto Spa
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;

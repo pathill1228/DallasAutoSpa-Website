516a286001b4b41867c7cb76a7f9f93275c74406
pathill1 <pathill1228@gmail.com>
2025-06-27 11:42:31 -0500

    Fixing href in nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -22,17 +22,17 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="../pages/about">
+              <a className="nav-link" href="/About">
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/services">
+              <a className="nav-link" href="/Services">
                 Services
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact">
+              <a className="nav-link" href="/Contact">
                 Contact
               </a>
             </li>

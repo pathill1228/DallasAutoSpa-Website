038b988298b97be52fa9b7507e2f948a40e48af4
pathill1 <pathill1228@gmail.com>
2025-06-29 13:21:15 -0500

    text color change

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,7 +3,7 @@ import "../Styles/Welcome.css";
 
 function Welcome() {
   return (
-    <div className="centered-container">
+    <div className="centered-container text-bg-dark p-3">
       <h1>Dallas Auto Spa</h1>
     </div>
   );

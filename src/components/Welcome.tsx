6ef4811b5e219154951a7d10306b947cf2c48783
pathill1 <pathill1228@gmail.com>
2025-06-29 13:14:40 -0500

    adding welcome

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "../Styles/Welcome.css";
+
+function Welcome() {
+  return <h1>Dallas Auto Spa</h1>;
+}
+
+export default Welcome;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/NavBar";
 import SlideShow from "../components/SlideShow";
+import Welcome from "../components/Welcome";
 
 function Home() {
   return (
     <div>
       <NavBar />
       <SlideShow />
+      <Welcome />
     </div>
   );
 }

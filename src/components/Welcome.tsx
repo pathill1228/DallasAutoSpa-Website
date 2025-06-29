3f7001601fd80c418ec7220855e484d41befe3d7
pathill1 <pathill1228@gmail.com>
2025-06-29 13:18:33 -0500

    centering content

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,7 +2,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../Styles/Welcome.css";
 
 function Welcome() {
-  return <h1>Dallas Auto Spa</h1>;
+  return (
+    <div className="centered-container">
+      <h1>Dallas Auto Spa</h1>
+    </div>
+  );
 }
 
 export default Welcome;

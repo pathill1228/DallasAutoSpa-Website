e953b28d091ef5b702550d837662831d181731bb
pathill1 <pathill1228@gmail.com>
2025-06-30 12:17:40 -0500

    br change

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,10 +5,14 @@ import { Link } from "react-router-dom";
 function Welcome() {
   return (
     <div className="centered-container">
-      <h2>DALLAS AUTO SPA<br></h2>
+      <h2>
+        DALLAS AUTO SPA
+        <br />
+      </h2>
       <h5>
         Welcome to Dallas Auto Spa. From Vinyl Wraps to Ceramic Coatings, We
-        Elivate Your Vehicle's Look and Preserve Its Value.<br>
+        Elivate Your Vehicle's Look and Preserve Its Value.
+        <br />
       </h5>
       <button type="button" className="btn btn-dark">
         <Link to="/Services" className="nav-link">

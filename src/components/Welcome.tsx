c0bcf4b26973308dbedf0e3685c5ac2d85d9b437
pathill1 <pathill1228@gmail.com>
2025-06-30 12:07:01 -0500

    call to action attempt

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,6 +5,15 @@ function Welcome() {
   return (
     <div className="centered-container">
       <h2>DALLAS AUTO SPA</h2>
+      <h5>
+        Welcome to Dallas Auto Spa. From Vinyl Wraps to Ceramic Coatings, We
+        Elivate Your Vehicle's Look and Preserve Its Value.
+      </h5>
+      <button type="button" className="btn btn-dark">
+        <Link to="/Services" className="nav-link">
+          See Our Services
+        </Link>
+      </button>
     </div>
   );
 }

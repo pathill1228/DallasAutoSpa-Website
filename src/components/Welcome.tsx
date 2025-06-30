7cb35c73b71a5f66e8d5d793a8c22a01f56e18fc
pathill1 <pathill1228@gmail.com>
2025-06-30 12:13:06 -0500

    adding breaks in welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,10 +6,12 @@ function Welcome() {
   return (
     <div className="centered-container">
       <h2>DALLAS AUTO SPA</h2>
+      <br></br>
       <h5>
         Welcome to Dallas Auto Spa. From Vinyl Wraps to Ceramic Coatings, We
         Elivate Your Vehicle's Look and Preserve Its Value.
       </h5>
+      <br></br>
       <button type="button" className="btn btn-dark">
         <Link to="/Services" className="nav-link">
           See Our Services
